699f45b908dd5a755db6e22523a47e509a9a4e40
kirill <kirias14@gmail.com>
2024-12-29 14:00:40 +0100

    Day23p2

--- Day23.py
+++ Day23.py
@@ -4,6 +4,7 @@ start_time = time.time()
 
 
 connections = set()
+connections_all = set()
 comps = set()
 
 with open('inputs/23.txt', 'r') as file:
@@ -14,6 +15,8 @@ with open('inputs/23.txt', 'r') as file:
             comps.add(con1)
             comps.add(con2)
             connections.add((con1,con2))
+            connections_all.add((con1,con2))
+            connections_all.add((con2,con1))
 
 triples = set()
 for conn in connections:
@@ -32,4 +35,40 @@ for con1, con2, con3 in triples:
 
 print(f"Part 1: {triples_with_t // 3}") # 1512
 
-print(f"Time: {time.time() - start_time}") # 3.3168
+parties = []
+for c in connections:
+    party = set()
+    party.add(c[0])
+    party.add(c[1])
+    parties.append(party)
+
+
+
+added = True
+while added:
+    added = False
+    for comp in comps:
+        for party in parties:
+            if comp in party:
+                continue
+            all_have_connections = True
+            for p in party:
+                if (p, comp) not in connections_all:
+                    all_have_connections = False
+                    break
+            if all_have_connections:
+                party.add(comp)
+                added = True
+
+max_party_len = max([len(x) for x in parties])
+max_party = None
+for party in parties:
+    if len(party) == max_party_len:
+        max_party = party
+        break
+
+part2 = ','.join(sorted(max_party))
+
+print(f"Part 1: {part2}") # ac,ed,fh,kd,lf,mb,om,pe,qt,uo,uy,vr,wg
+
+print(f"Time: {time.time() - start_time}") # 0.7240538
